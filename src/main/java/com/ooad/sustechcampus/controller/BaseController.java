663cb46db830f06ddc5bb5c9b1a3e89b9c262b9e
evan3 <evan3li@outlook.com>
2023-09-22 11:47:17 +0800

    Initial commit

---
+++ src/main/java/com/ooad/sustechcampus/controller/BaseController.java
@@ -0,0 +1,8 @@
+package com.ooad.sustechcampus.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BaseController {
+//    final IRedisUtils redisUtils;
+}
---
+++ src/test/java/com/ooad/sustechcampus/SusTechCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooad.sustechcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusTechCampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
