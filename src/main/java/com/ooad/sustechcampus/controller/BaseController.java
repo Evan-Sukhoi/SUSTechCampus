f57c4de09687d5d2581757aa1e3637096f849cb0
evan3 <evan3li@outlook.com>
2023-09-24 23:31:26 +0800

    【重要】更改了文件结构，以采用了微服务架构

--- src/main/java/com/ooad/sustechcampus/controller/BaseController.java
+++
@@ -1,8 +0,0 @@
-package com.ooad.sustechcampus.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class BaseController {
-//    final IRedisUtils redisUtils;
-}
--- src/main/java/com/ooad/sustechcampus/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ooad.sustechcampus.dao;
-
-import com.ooad.sustechcampus.model.User;
-
-import java.util.List;
-
-public interface UserDAO {
-    List<User> getUserList();
-}
