0ef6a5b5bb873e329441b5481da9f9d5ded28ce2
wiki <12110411@mail.sustech.edu.cn>
2023-10-08 14:58:42 +0800

    add sidebar in building details

--- frontend/user/src/components/BuildingDetails.vue
+++ frontend/user/src/components/BuildingDetails.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class="container">
     <div id="buildingName">
       <h3 style="text-align: center">
         hello world
       </h3>
     </div>
 
-    <div>
+    <div id="ppt">
       <el-carousel :interval="4000" type="card" height="200px">
         <el-carousel-item v-for="item in 4" :key="item">
           <h3 text="2xl" justify="center">{{ item }}</h3>
@@ -14,23 +14,72 @@
       </el-carousel>
     </div>
 
-    <BuildingBlog></BuildingBlog>
+    <div id="detail">
+      <div class="sidebar">
+        <vs-sidebar
+            relative
+            hover-expand
+            reduce
+            v-model="active"
+            open
+        >
+          <vs-sidebar-item id="home">
+            <template #icon>
+              <i class='bx bx-home'></i>
+            </template>
+            Home
+          </vs-sidebar-item>
+
+          <vs-sidebar-item id="market">
+            <template #icon>
+              <i class='bx bx-grid-alt'></i>
+            </template>
+            Market Overview
+          </vs-sidebar-item>
+
+          <vs-sidebar-item id="Music">
+            <template #icon>
+              <i class='bx bxs-music'></i>
+            </template>
+            Music
+          </vs-sidebar-item>
+
+          <vs-sidebar-item id="donate">
+            <template #icon>
+              <i class='bx bxs-donate-heart' ></i>
+            </template>
+            Donate
+          </vs-sidebar-item>
+
+          <vs-sidebar-item id="drink">
+            <template #icon>
+              <i class='bx bx-drink'></i>
+            </template>
+            Drink
+          </vs-sidebar-item>
+
+          <vs-sidebar-item id="shopping">
+            <template #icon>
+              <i class='bx bxs-shopping-bags'></i>
+            </template>
+            Shopping
+          </vs-sidebar-item>
+        </vs-sidebar>
+      </div>
+    </div>
 
   </div>
 
 </template>
 
 <script>
-import BuildingBlog from './BuildingBlog.vue'
 export default {
   name: "buildingDetails",
-  mounted() {
-    const buildingName = this.$route.params.id;
-    console.log(buildingName);
-  },
   components: {
-    BuildingBlog,
-  }
+  },
+  data:() => ({
+    active: 'home',
+  })
 }
 </script>
 
@@ -50,4 +99,15 @@ export default {
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
 }
+
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+
+
+
+
+
 </style>
