eb76cd3ae8fe7c1b2d6ba3a12b4db09cd3b99b98
jimmylaw21 <1908486736@qq.com>
2023-10-13 15:42:10 +0800

    更新了database的dao和po，罗皓予，2023/10/13 15:42

--- backend/database/src/main/java/com/sustech/campus/database/dao/imageDao.java
+++
@@ -1,7 +0,0 @@
-package com.sustech.campus.database.dao;
-
-import com.github.yulichang.base.MPJBaseMapper;
-import com.sustech.campus.database.po.image;
-
-public interface imageDao extends MPJBaseMapper<image>{
-}
