a63e125b10cb20e185b8b7e8bb28727b83f2efc1
wiki <12110411@mail.sustech.edu.cn>
2023-10-14 13:56:28 +0800

    gaode api

--- frontend/user/src/components/userComponents/SUSMap.vue
+++ frontend/user/src/components/userComponents/SUSMap.vue
@@ -1,30 +1,96 @@
 <template>
-  <div>
-    <h3 class="title">{{msg}}</h3>
-    <div class="amap-wrapper">
-      <el-amap class="amap-box" :vid="'amap-vue'"></el-amap>
-    </div>
+  <div class="map">
+    <div id="container"></div>
+    <div id="panel"></div>
   </div>
 </template>
-
 <script>
-export default {
-  data () {
-    return {
-      msg: 'vue-amap向你问好'
-    }
-  }
+import AMapLoader from "@amap/amap-jsapi-loader";
+
+window._AMapSecurityConfig = {
+  securityJsCode: '0631f670d1dec9cd73d8d4dca2075792',
 }
+export default {
+  name: "SUSMap",
+  data() {
+    return {
+      driving: '',
+    }
+  },
+  mounted() {
+    this.initAMap();
+  },
+  unmounted() {
+    this.map?.destroy();
+  },
+  methods: {
+    initAMap() {
+      AMapLoader.load({
+        key: "d647a0c3b5f5de68bc23204f7365fc97", // 申请好的Web端开发者Key，首次调用 load 时必填
+        version: "1.4.15", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+        plugins: ['AMap.Driving', 'AMap.LngLat', 'AMap.Walking'], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+      })
+          .then((AMap) => {
+            this.AMap = AMap;
+            this.map = new this.AMap.Map("container", {
+              viewMode: "2D",
+              zoom: 18,
+              center: [114.000725, 22.595509],
+            });
+            this.driving = new this.AMap.Driving({
+              map: this.map,
+              panel: "panel"
+            });
+            this.map.on("click", this.handleMapClick.bind(this));
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+
+    },
+
+    findDrivingWay([a, b], [c, d]) {
+      this.driving.search(new this.AMap.LngLat(a, b), new this.AMap.LngLat(c, d), function (status, result) {
+        console.log("start")
+        if (status === 'complete') {
+          console.log('绘制驾车路线完成')
+        } else {
+          console.log('获取驾车数据失败：' + result)
+        }
+      });
+    },
+
+    handleMapClick(e) {
+      // e.lnglat 获取点击位置的经纬度
+      var lng = e.lnglat.getLng();
+      var lat = e.lnglat.getLat();
+      console.log("你点击了地图在经度" + lng + "，纬度" + lat + "的位置");
+    }
+
+
+
+  },
+
+
+};
 
 </script>
-<style>
-.amap-wrapper {
-  width: 50%;
-  height: 500px;
+<style scoped>
+.map {
+  position: relative;
 }
 
-.amap-box {
+#container {
   width: 100%;
-  height: 100%;
+  height: 800px;
+}
+
+#panel {
+  position: absolute;
+  right: 10%;
+  bottom: 80%;
+  width: 30%;
+  height: 100px;
+  z-index: 100;
 }
 </style>
--- frontend/user/src/main.js
+++ frontend/user/src/main.js
@@ -8,7 +8,7 @@ import 'vuesax/dist/vuesax.css'
 import 'boxicons'
 import 'boxicons/css/boxicons.css'
 import VueI18n from 'vue-i18n'
-import VueAMap from 'vue-amap';
+
 
 
 
@@ -19,13 +19,9 @@ Vue.use(Element)
 Vue.use(VueSax)
 Vue.use(VueI18n)
 Vue.use(VueI18n);
-Vue.use(VueAMap);
 
-VueAMap.initAMapApiLoader({
-  key: 'd647a0c3b5f5de68bc23204f7365fc97',
-  plugin: ['AMap.Driving', 'AMap.Walking',],
-  v: '1.4.15'
-});
+
+
 
 let lang = localStorage.getItem('language') || 'en-US';
 const i18n = new VueI18n({
