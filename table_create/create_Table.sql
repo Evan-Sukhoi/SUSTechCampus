9e53f93b2de6e8e2c71f5bacbbc52938272b0f90
evan3 <evan3li@outlook.com>
2023-11-13 22:17:55 +0800

    建表语句做出了一些改进

--- table_create/create_Table.sql
+++ table_create/create_Table.sql
@@ -1,19 +1,32 @@
-git /*
+/*
 假设:
 所有的varchar默认不会超过255个字符
 长文本（比如introduction使用text）
-所有的时间都是TIMESTAMP
+所有的时间都是以24小时制表示
 所有的图片和视频都以BLOB类型存储
 所有的分数都是小数，最多有两位小数
 所有的表都没有外键
 所有的表都有主键
 */
 
+-- drop all tables
+# DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Admins;
+DROP TABLE IF EXISTS Blacklist;
+DROP TABLE IF EXISTS Building;
+DROP TABLE IF EXISTS Room;
+DROP TABLE IF EXISTS Room_type;
+DROP TABLE IF EXISTS bus_line;
+DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Reservation;
+DROP TABLE IF EXISTS Users;
+
 -- 用户表
-CREATE TABLE User (
+CREATE TABLE user
+(
     user_ID INT AUTO_INCREMENT,
-    name VARCHAR(255),
     phone VARCHAR(255),
+    name VARCHAR(255),
     email VARCHAR(255),
     password VARCHAR(255),
     image BLOB,
@@ -21,26 +34,26 @@ CREATE TABLE User (
 );
 
 -- 管理员表
-CREATE TABLE Admin (
-    admin_ID INT AUTO_INCREMENT,
-    name VARCHAR(255),
-    phone VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    image BLOB,
-    PRIMARY KEY (admin_ID)
+CREATE TABLE admin (
+                       admin_ID INT AUTO_INCREMENT,
+                       name VARCHAR(255),
+                       phone VARCHAR(255),
+                       email VARCHAR(255),
+                       password VARCHAR(255),
+                       image BLOB,
+                       PRIMARY KEY (admin_ID)
 );
 
 -- 黑名单表
-CREATE TABLE Blacklist (
-    list_ID INT AUTO_INCREMENT,
-    user_ID INT,
-    admin_ID INT,
-    PRIMARY KEY (list_ID)
+CREATE TABLE blacklist (
+                           list_ID INT AUTO_INCREMENT,
+                           user_ID INT,
+                           admin_ID INT,
+                           PRIMARY KEY (list_ID)
 );
 
 -- 建筑表
-CREATE TABLE Building (
+CREATE TABLE building (
                           building_ID INT AUTO_INCREMENT,
                           name VARCHAR(255),
                           open_time TIME,
@@ -53,75 +66,66 @@ CREATE TABLE Building (
                           PRIMARY KEY (building_ID)
 );
 
--- 建筑对应图片 一对多需要新建表
-CREATE TABLE Buildings_Image (
-    building_ID INT,
-    image_ID INT
-);
-
 -- 房间表
-CREATE TABLE Room (
-    room_ID INT AUTO_INCREMENT,
-    building_ID INT,
-    number INT,
-    room_type_ID INT,
-    PRIMARY KEY (room_ID)
+CREATE TABLE room (
+                      room_ID INT AUTO_INCREMENT,
+                      building_ID INT,
+                      number INT,
+                      room_type_ID INT,
+                      PRIMARY KEY (room_ID)
 );
 
 -- 房间类型表
-CREATE TABLE Room_type (
-    room_type_ID INT AUTO_INCREMENT,
-    type VARCHAR(255),
-    capacity INT,
-    PRIMARY KEY (room_type_ID)
+CREATE TABLE room_type (
+                           room_type_ID INT AUTO_INCREMENT,
+                           type VARCHAR(255),
+                           image BLOB,
+                           capacity INT,
+                           PRIMARY KEY (room_type_ID)
 );
--- Room_type对应图片 一对多需要新建表
-CREATE TABLE Room_type_Image (
-    room_type_ID INT,
-    image_ID INT
-);
-
 
 -- 巴士线路表
-CREATE TABLE Bus_line (
-   bus_line_ID INT AUTO_INCREMENT,
-   line_ID INT,
-   station VARCHAR(255),
-   _index_ INT,
-   PRIMARY KEY (bus_line_ID)
+CREATE TABLE bus_line (
+                          bus_line_ID INT AUTO_INCREMENT,
+                          line_ID INT,
+                          station VARCHAR(255),
+                          _index_ INT,
+                          PRIMARY KEY (bus_line_ID)
 );
-
-
 
 -- 评价表
-CREATE TABLE Comment (
-   comment_ID INT AUTO_INCREMENT,
-   user_ID INT,
-   time TIMESTAMP,
-   text TEXT,
-   building_ID INT,
-   score DECIMAL(3,2),
-   admin_ID INT,
-   PRIMARY KEY (comment_ID)
-);
--- comment_ID对应图片 一对多需要新建表
-CREATE TABLE Comment_ID_Image (
-    comment_ID INT,
-    image_ID INT
-);
-
--- 创建图片表
-CREATE TABLE Image (
-    image_ID INT AUTO_INCREMENT PRIMARY KEY,
-    image BLOB
+CREATE TABLE comment (
+                         comment_ID INT AUTO_INCREMENT,
+                         user_ID INT,
+                         time TIMESTAMP,
+                         text TEXT,
+                         image BLOB,
+                         building_ID INT,
+                         score DECIMAL(3,2),
+                         admin_ID INT,
+                         PRIMARY KEY (comment_ID)
 );
 
 -- 预订表
-CREATE TABLE Reservation (
-   Reservation_ID INT AUTO_INCREMENT,
-   room_ID INT,
-   start_time TIMESTAMP,
-   end_time TIMESTAMP,
-   user_ID INT,
-   PRIMARY KEY (Reservation_ID)
+CREATE TABLE reservation (
+                             Reservation_ID INT AUTO_INCREMENT,
+                             room_ID INT,
+                             start_time TIMESTAMP,
+                             end_time TIMESTAMP,
+                             user_ID INT,
+                             PRIMARY KEY (Reservation_ID)
 );
+
+
+insert into Building (name, open_time, close_time, location_name, introduction, nearest_station, video_url, cover_ID) values ('琳恩图书馆', '08:00:00', '22:00:00', '琳恩图书馆', '琳恩图书馆是一个图书馆', '行政楼', 'https://www.bilibili.com/video/BV1Y7411H7jZ', 1);
+insert into Room (building_ID, number, room_type_ID) values (1, 101, 1);
+insert into Room (building_ID, number, room_type_ID) values (1, 102, 1);
+insert into Room (building_ID, number, room_type_ID) values (1, 103, 1);
+
+
+insert into User(phone, name, email, password) values ('15138688888', 'evan', 'evan3li@outlook.com', '123456');
+insert into User(phone, name, email, password) values ('16138688888', 'ln', 'example@gmail.com', '123456');
+insert into User(phone, name, email, password) values ('17138688888', 'jimmy', '12112517@mail.sustech.edu.cn', '123456');
+insert into User(phone, name, email, password) values ('18138688888', 'juan', '111@abc.com', '123456');
+
+insert into Admin(name, phone, email, password) values ('admin', '15138688888', 'evan3li@outlook.com', '123456');
