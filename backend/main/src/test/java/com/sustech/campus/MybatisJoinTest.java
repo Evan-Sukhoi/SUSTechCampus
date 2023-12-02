9c850fff36df77ba335bc67b2921a9587cf96bc9
LN <12110416@mail.sustech.edu.cn>
2023-12-03 04:29:37 +0800

    未完成支付api 沙箱

--- backend/main/src/test/java/com/sustech/campus/MybatisJoinTest.java
+++ backend/main/src/test/java/com/sustech/campus/MybatisJoinTest.java
@@ -1,37 +1,37 @@
-package com.sustech.campus;
-
-import com.github.yulichang.wrapper.MPJLambdaWrapper;
-import com.sustech.campus.database.dao.RoomDao;
-import com.sustech.campus.database.po.Building;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.baomidou.mybatisplus.core.toolkit.Assert;
-import com.sustech.campus.database.dao.RoomDao;
-import com.sustech.campus.model.vo.RoomInfo;
-import com.sustech.campus.database.po.Room;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-@SpringBootTest
-public class MybatisJoinTest {
-
-    @Autowired
-    private RoomDao roomDao;
-
-    @Test
-    public void testSelect() {
-        MPJLambdaWrapper<Room> wrapper = new MPJLambdaWrapper<Room>()
-                .selectAll(Room.class)
-                .select(Building::getName, Building::getBuilding_id)
-                .leftJoin(Building.class, Building::getBuilding_id, Room::getBuilding_id);
-
-        List<RoomInfo> userList = roomDao.selectJoinList(RoomInfo.class, wrapper);
-
-        userList.forEach(System.out::println);
-    }
-
-}
-
+//package com.sustech.campus;
+//
+//import com.github.yulichang.wrapper.MPJLambdaWrapper;
+//import com.sustech.campus.database.dao.RoomDao;
+//import com.sustech.campus.database.po.Building;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.baomidou.mybatisplus.core.toolkit.Assert;
+//import com.sustech.campus.database.dao.RoomDao;
+//import com.sustech.campus.model.vo.RoomInfo;
+//import com.sustech.campus.database.po.Room;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+//@SpringBootTest
+//public class MybatisJoinTest {
+//
+//    @Autowired
+//    private RoomDao roomDao;
+//
+//    @Test
+//    public void testSelect() {
+//        MPJLambdaWrapper<Room> wrapper = new MPJLambdaWrapper<Room>()
+//                .selectAll(Room.class)
+//                .select(Building::getName, Building::getBuilding_id)
+//                .leftJoin(Building.class, Building::getBuilding_id, Room::getBuilding_id);
+//
+//        List<RoomInfo> userList = roomDao.selectJoinList(RoomInfo.class, wrapper);
+//
+//        userList.forEach(System.out::println);
+//    }
+//
+//}
+//
